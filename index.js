e3ef8588a919223df1eab896fc2bd97d69714c76
Zach Frohardt <zfrohardt@gmail.com>
2021-04-05 12:18:53 -0700

    commit tree 9bc92a3f183a7eeacd83ac9c3d026cdce450257e

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 9;
+let num4 = 5;
+let mod = num3 % num4;
+
+let max = Math.max(1, 2, 3, 20, 5, 6);
